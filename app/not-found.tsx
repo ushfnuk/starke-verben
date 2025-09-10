9a56743d12503d876e7f5a9195004a46a909cafd
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-09-10 22:04:11 +0200

    Not-found questions

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <main className="grid place-content-center h-screen w-screen">
+      <h1 className="text-4xl text-error-content font-semibold p-5">Куди ото воно лізе, га?!</h1>
+    </main>
+  )
+}
+
+export default NotFoundPage
