d1f7fd13e7f0133add53987a9e70606482a73be0
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-09-10 17:46:11 +0200

    Fetch data from SQLite database

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { prisma } from "@/prisma/client";
+
+export default async function Home() {
+  const verben = await prisma.verben.findMany();
+
   return (
     <main>
       <table className='table table-md'>
@@ -12,124 +16,15 @@ export default function Home() {
           </tr>
         </thead>
         <tbody>
-          <tr><td>abbiegen</td><td>biegt ab</td><td>bog ab</td><td>ist abgebogen</td><td>повертати</td></tr>
-          <tr><td>backen</td><td>bäckt / backt</td><td>buk / backte</td><td>hat gebacken</td><td>пекти</td></tr>
-          <tr><td>beginnen</td><td>beginnt</td><td>begann</td><td>hat begonnen</td><td>починати</td></tr>
-          <tr><td>beißen</td><td>beißt</td><td>biss</td><td>hat gebissen</td><td>кусати</td></tr>
-          <tr><td>beweisen</td><td>beweist</td><td>bewies</td><td>hat bewiesen</td><td>доводити</td></tr>
-          <tr><td>bewerben (sich)</td><td>bewirbt (sich)</td><td>bewarb (sich)</td><td>hat (sich) beworben</td><td>подавати заявку</td></tr>
-          <tr><td>biegen</td><td>biegt</td><td>bog</td><td>hat gebogen</td><td>гнути</td></tr>
-          <tr><td>bieten</td><td>bietet</td><td>bot</td><td>hat geboten</td><td>пропонувати</td></tr>
-          <tr><td>binden</td><td>bindet</td><td>band</td><td>hat gebunden</td><td>зв’язувати</td></tr>
-          <tr><td>bitten</td><td>bittet</td><td>bat</td><td>hat gebeten</td><td>просити</td></tr>
-          <tr><td>bleiben</td><td>bleibt</td><td>blieb</td><td>ist geblieben</td><td>залишатися</td></tr>
-          <tr><td>braten</td><td>brät</td><td>briet</td><td>hat gebraten</td><td>смажити</td></tr>
-          <tr><td>brechen</td><td>bricht</td><td>brach</td><td>hat / ist gebrochen</td><td>ламати</td></tr>
-          <tr><td>brennen</td><td>brennt</td><td>brannte</td><td>hat gebrannt</td><td>горіти</td></tr>
-          <tr><td>bringen</td><td>bringt</td><td>brachte</td><td>hat gebracht</td><td>приносити</td></tr>
-          <tr><td>denken</td><td>denkt</td><td>dachte</td><td>hat gedacht</td><td>думати</td></tr>
-          <tr><td>empfangen</td><td>empfängt</td><td>empfing</td><td>hat empfangen</td><td>отримувати</td></tr>
-          <tr><td>empfehlen</td><td>empfiehlt</td><td>empfahl</td><td>hat empfohlen</td><td>рекомендувати</td></tr>
-          <tr><td>empfinden</td><td>empfindet</td><td>empfand</td><td>hat empfunden</td><td>відчувати</td></tr>
-          <tr><td>entscheiden</td><td>entscheidet</td><td>entschied</td><td>hat entschieden</td><td>вирішувати</td></tr>
-          <tr><td>erschrecken</td><td>erschrickt</td><td>erschrak</td><td>ist erschrocken</td><td>лякатися</td></tr>
-          <tr><td>essen</td><td>isst</td><td>aß</td><td>hat gegessen</td><td>їсти</td></tr>
-          <tr><td>fahren</td><td>fährt</td><td>fuhr</td><td>ist / hat gefahren</td><td>їхати, керувати</td></tr>
-          <tr><td>fallen</td><td>fällt</td><td>fiel</td><td>ist gefallen</td><td>падати</td></tr>
-          <tr><td>fangen</td><td>fängt</td><td>fing</td><td>hat gefangen</td><td>ловити</td></tr>
-          <tr><td>finden</td><td>findet</td><td>fand</td><td>hat gefunden</td><td>знаходити</td></tr>
-          <tr><td>fliegen</td><td>fliegt</td><td>flog</td><td>ist / hat geflogen</td><td>летіти</td></tr>
-          <tr><td>fliehen</td><td>flieht</td><td>floh</td><td>ist geflohen</td><td>тікати</td></tr>
-          <tr><td>fließen</td><td>fließt</td><td>floss</td><td>ist geflossen</td><td>текти</td></tr>
-          <tr><td>fressen</td><td>frisst</td><td>fraß</td><td>hat gefressen</td><td>їсти (про тварин)</td></tr>
-          <tr><td>frieren</td><td>friert</td><td>fror</td><td>hat gefroren</td><td>мерзнути</td></tr>
-          <tr><td>geben</td><td>gibt</td><td>gab</td><td>hat gegeben</td><td>давати</td></tr>
-          <tr><td>gefallen</td><td>gefällt</td><td>gefiel</td><td>hat gefallen</td><td>подобатися</td></tr>
-          <tr><td>gehen</td><td>geht</td><td>ging</td><td>ist gegangen</td><td>йти</td></tr>
-          <tr><td>gelingen</td><td>gelingt</td><td>gelang</td><td>ist gelungen</td><td>вдаватися</td></tr>
-          <tr><td>gelten</td><td>gilt</td><td>galt</td><td>hat gegolten</td><td>діяти, вважатися</td></tr>
-          <tr><td>genießen</td><td>genießt</td><td>genoss</td><td>hat genossen</td><td>насолоджуватися</td></tr>
-          <tr><td>geraten</td><td>gerät</td><td>geriet</td><td>ist geraten</td><td>потрапляти</td></tr>
-          <tr><td>geschehen</td><td>geschieht</td><td>geschah</td><td>ist geschehen</td><td>траплятися</td></tr>
-          <tr><td>gewinnen</td><td>gewinnt</td><td>gewann</td><td>hat gewonnen</td><td>вигравати</td></tr>
-          <tr><td>gießen</td><td>gießt</td><td>goss</td><td>hat gegossen</td><td>наливати, поливати</td></tr>
-          <tr><td>greifen</td><td>greift</td><td>griff</td><td>hat gegriffen</td><td>хапати</td></tr>
-          <tr><td>haben</td><td>hat</td><td>hatte</td><td>hat gehabt</td><td>мати</td></tr>
-          <tr><td>halten</td><td>hält</td><td>hielt</td><td>hat gehalten</td><td>тримати</td></tr>
-          <tr><td>hängen</td><td>hängt</td><td>hing</td><td>hat gehangen</td><td>висіти</td></tr>
-          <tr><td>heben</td><td>hebt</td><td>hob</td><td>hat gehoben</td><td>піднімати</td></tr>
-          <tr><td>heißen</td><td>heißt</td><td>hieß</td><td>hat geheißen</td><td>називатися</td></tr>
-          <tr><td>helfen</td><td>hilft</td><td>half</td><td>hat geholfen</td><td>допомагати</td></tr>
-          <tr><td>kennen</td><td>kennt</td><td>kannte</td><td>hat gekannt</td><td>знати</td></tr>
-          <tr><td>klingen</td><td>klingt</td><td>klang</td><td>hat geklungen</td><td>звучати</td></tr>
-          <tr><td>kommen</td><td>kommt</td><td>kam</td><td>ist gekommen</td><td>приходити</td></tr>
-          <tr><td>laden</td><td>lädt</td><td>lud</td><td>hat geladen</td><td>вантажити, запрошувати</td></tr>
-          <tr><td>lassen</td><td>lässt</td><td>ließ</td><td>hat gelassen</td><td>залишати, дозволяти</td></tr>
-          <tr><td>laufen</td><td>läuft</td><td>lief</td><td>ist gelaufen</td><td>бігти</td></tr>
-          <tr><td>leiden</td><td>leidet</td><td>litt</td><td>hat gelitten</td><td>страждати</td></tr>
-          <tr><td>leihen</td><td>leiht</td><td>lieh</td><td>hat geliehen</td><td>позичати</td></tr>
-          <tr><td>lesen</td><td>liest</td><td>las</td><td>hat gelesen</td><td>читати</td></tr>
-          <tr><td>liegen</td><td>liegt</td><td>lag</td><td>hat gelegen</td><td>лежати</td></tr>
-          <tr><td>lügen</td><td>lügt</td><td>log</td><td>hat gelogen</td><td>брехати</td></tr>
-          <tr><td>messen</td><td>misst</td><td>maß</td><td>hat gemessen</td><td>вимірювати</td></tr>
-          <tr><td>nehmen</td><td>nimmt</td><td>nahm</td><td>hat genommen</td><td>брати</td></tr>
-          <tr><td>nennen</td><td>nennt</td><td>nannte</td><td>hat genannt</td><td>називати</td></tr>
-          <tr><td>pfeifen</td><td>pfeift</td><td>pfiff</td><td>hat gepfiffen</td><td>свистіти</td></tr>
-          <tr><td>raten</td><td>rät</td><td>riet</td><td>hat geraten</td><td>радити</td></tr>
-          <tr><td>reiten</td><td>reitet</td><td>ritt</td><td>ist / hat geritten</td><td>їздити верхи</td></tr>
-          <tr><td>rennen</td><td>rennt</td><td>rannte</td><td>ist gerannt</td><td>бігти</td></tr>
-          <tr><td>riechen</td><td>riecht</td><td>roch</td><td>hat gerochen</td><td>пахнути</td></tr>
-          <tr><td>rufen</td><td>ruft</td><td>rief</td><td>hat gerufen</td><td>кликати</td></tr>
-          <tr><td>scheinen</td><td>scheint</td><td>schien</td><td>hat geschienen</td><td>світити, здаватися</td></tr>
-          <tr><td>schieben</td><td>schiebt</td><td>schob</td><td>hat geschoben</td><td>штовхати</td></tr>
-          <tr><td>schießen</td><td>schießt</td><td>schoss</td><td>hat geschossen</td><td>стріляти</td></tr>
-          <tr><td>schlafen</td><td>schläft</td><td>schlief</td><td>hat geschlafen</td><td>спати</td></tr>
-          <tr><td>schlagen</td><td>schlägt</td><td>schlug</td><td>hat geschlagen</td><td>бити</td></tr>
-          <tr><td>schließen</td><td>schließt</td><td>schloss</td><td>hat geschlossen</td><td>закривати</td></tr>
-          <tr><td>schmelzen</td><td>schmilzt</td><td>schmolz</td><td>ist / hat geschmolzen</td><td>танути</td></tr>
-          <tr><td>schneiden</td><td>schneidet</td><td>schnitt</td><td>hat geschnitten</td><td>різати</td></tr>
-          <tr><td>schreiben</td><td>schreibt</td><td>schrieb</td><td>hat geschrieben</td><td>писати</td></tr>
-          <tr><td>schreien</td><td>schreit</td><td>schrie</td><td>hat geschrien</td><td>кричати</td></tr>
-          <tr><td>schweigen</td><td>schweigt</td><td>schwieg</td><td>hat geschwiegen</td><td>мовчати</td></tr>
-          <tr><td>schwimmen</td><td>schwimmt</td><td>schwamm</td><td>ist / hat geschwommen</td><td>плавати</td></tr>
-          <tr><td>sehen</td><td>sieht</td><td>sah</td><td>hat gesehen</td><td>бачити</td></tr>
-          <tr><td>sein</td><td>ist</td><td>war</td><td>ist gewesen</td><td>бути</td></tr>
-          <tr><td>senden</td><td>sendet</td><td>sandte / sendete</td><td>hat gesandt / gesendet</td><td>надсилати</td></tr>
-          <tr><td>singen</td><td>singt</td><td>sang</td><td>hat gesungen</td><td>співати</td></tr>
-          <tr><td>sinken</td><td>sinkt</td><td>sank</td><td>ist gesunken</td><td>тонути</td></tr>
-          <tr><td>sitzen</td><td>sitzt</td><td>saß</td><td>hat gesessen</td><td>сидіти</td></tr>
-          <tr><td>sprechen</td><td>spricht</td><td>sprach</td><td>hat gesprochen</td><td>говорити</td></tr>
-          <tr><td>springen</td><td>springt</td><td>sprang</td><td>ist gesprungen</td><td>стрибати</td></tr>
-          <tr><td>stehen</td><td>steht</td><td>stand</td><td>hat gestanden</td><td>стояти</td></tr>
-          <tr><td>stehlen</td><td>stiehlt</td><td>stahl</td><td>hat gestohlen</td><td>красти</td></tr>
-          <tr><td>steigen</td><td>steigt</td><td>stieg</td><td>ist gestiegen</td><td>підніматися</td></tr>
-          <tr><td>sterben</td><td>stirbt</td><td>starb</td><td>ist gestorben</td><td>померти</td></tr>
-          <tr><td>stoßen</td><td>stößt</td><td>stieß</td><td>hat / ist gestoßen</td><td>штовхати, натикатися</td></tr>
-          <tr><td>streichen</td><td>streicht</td><td>strich</td><td>hat gestrichen</td><td>фарбувати, закреслювати</td></tr>
-          <tr><td>streiten</td><td>streitet</td><td>stritt</td><td>hat gestritten</td><td>сваритися</td></tr>
-          <tr><td>tragen</td><td>trägt</td><td>trug</td><td>hat getragen</td><td>носити</td></tr>
-          <tr><td>treffen</td><td>trifft</td><td>traf</td><td>hat getroffen</td><td>зустрічати</td></tr>
-          <tr><td>treiben</td><td>treibt</td><td>trieb</td><td>hat getrieben</td><td>гнати, займатися (спортом)</td></tr>
-          <tr><td>treten</td><td>tritt</td><td>trat</td><td>ist / hat getreten</td><td>ступати, пнути</td></tr>
-          <tr><td>trinken</td><td>trinkt</td><td>trank</td><td>hat getrunken</td><td>пити</td></tr>
-          <tr><td>tun</td><td>tut</td><td>tat</td><td>hat getan</td><td>робити</td></tr>
-          <tr><td>verbieten</td><td>verbietet</td><td>verbot</td><td>hat verboten</td><td>забороняти</td></tr>
-          <tr><td>vergessen</td><td>vergisst</td><td>vergaß</td><td>hat vergessen</td><td>забувати</td></tr>
-          <tr><td>vergleichen</td><td>vergleicht</td><td>verglich</td><td>hat verglichen</td><td>порівнювати</td></tr>
-          <tr><td>verlieren</td><td>verliert</td><td>verlor</td><td>hat verloren</td><td>втрачати</td></tr>
-          <tr><td>vermeiden</td><td>vermeidet</td><td>vermied</td><td>hat vermieden</td><td>уникати</td></tr>
-          <tr><td>verschwinden</td><td>verschwindet</td><td>verschwand</td><td>ist verschwunden</td><td>зникати</td></tr>
-          <tr><td>verzeihen</td><td>verzeiht</td><td>verzieh</td><td>hat verziehen</td><td>пробачати</td></tr>
-          <tr><td>wachsen</td><td>wächst</td><td>wuchs</td><td>ist gewachsen</td><td>рости</td></tr>
-          <tr><td>waschen</td><td>wäscht</td><td>wusch</td><td>hat gewaschen</td><td>мити, прати</td></tr>
-          <tr><td>weisen</td><td>weist</td><td>wies</td><td>hat gewiesen</td><td>вказувати</td></tr>
-          <tr><td>werben</td><td>wirbt</td><td>warb</td><td>hat geworben</td><td>рекламувати, агітувати</td></tr>
-          <tr><td>werden</td><td>wird</td><td>wurde</td><td>ist geworden</td><td>ставати</td></tr>
-          <tr><td>werfen</td><td>wirft</td><td>warf</td><td>hat geworfen</td><td>кидати</td></tr>
-          <tr><td>wiegen</td><td>wiegt</td><td>wog</td><td>hat gewogen</td><td>важити</td></tr>
-          <tr><td>wissen</td><td>weiß</td><td>wusste</td><td>hat gewusst</td><td>знати</td></tr>
-          <tr><td>ziehen</td><td>zieht</td><td>zog</td><td>hat / ist gezogen</td><td>тягнути, переїжджати</td></tr>
-          <tr><td>zwingen</td><td>zwingt</td><td>zwang</td><td>hat gezwungen</td><td>примушувати</td></tr>
+          {verben.map(verb =>
+            <tr key={verb.id}>
+              <td>{verb.infinitiv}</td>
+              <td>{verb.dritte_form}</td>
+              <td>{verb.preteritum}</td>
+              <td>{verb.perfekt}</td>
+              <td>{verb.ubersatz}</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </main>
---
+++ prisma/client.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@/app/generated/prisma/client';
+import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
+
+const adapter = new PrismaBetterSQLite3({
+  url: "file:./prisma/verben.db"
+});
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
