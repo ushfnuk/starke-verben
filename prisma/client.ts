402176b3c981a6802c481cd4db297eee79b5b4a7
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-09-24 19:44:11 +0200

    prisma/verben.db -> public/verben.db

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@/app/generated/prisma/client';
 import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
 
 const adapter = new PrismaBetterSQLite3({
-  url: "file:./prisma/verben.db"
+  url: "file:public/verben.db"
 });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
