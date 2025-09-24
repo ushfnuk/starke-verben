b16e4b9c38a850efb0dca7588462fd13d0e981d8
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-09-24 19:38:54 +0200

    Revert "./prisma/verben.db -> ./verben.db"
    This reverts commit 257b6611484fcc7de5d902a096b0d3efcb051ddb.

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@/app/generated/prisma/client';
 import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
 
 const adapter = new PrismaBetterSQLite3({
-  url: "file:./verben.db"
+  url: "file:./prisma/verben.db"
 });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
