788cd2c29f9bd60743ed20990ec180378341dfd6
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-12-08 12:28:45 +0100

    Updated NextJS and other dependencies

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@/app/generated/prisma/client';
-import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSQLite3({
-  url: "file:public/verben.db"
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL
 });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
